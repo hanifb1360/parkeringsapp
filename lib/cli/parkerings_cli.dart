c27441d43ed000f1ad2c3397c93a721a792e07e3
Hanif Bahari <hanif.bahari@gmail.com>
2024-10-01 00:13:13 +0200

    Added logic for manageParkingSpaces() and manageParkings()

--- lib/cli/parkerings_cli.dart
+++ lib/cli/parkerings_cli.dart
@@ -31,10 +31,10 @@ void runCLI() {
         manageVehicles(vehicleRepository, personRepository);
         break;
       case '3':
-        // Call the manageParkingSpaces() function here when implemented
+        manageParkingSpaces(parkingSpaceRepository);
         break;
       case '4':
-        // Call the manageParkings() function here when implemented
+        manageParkings(parkingRepository, vehicleRepository, parkingSpaceRepository);
         break;
       case '5':
         print('Avslutar...');
@@ -154,6 +154,133 @@ void manageVehicles(VehicleRepository vehicleRepo, PersonRepository personRepo) 
       var registrationNumber = stdin.readLineSync();
       vehicleRepo.delete(registrationNumber!);
       print('Fordon borttagen.');
+      break;
+    case '5':
+      return;
+    default:
+      print('Ogiltigt val, försök igen.');
+  }
+}
+
+void manageParkingSpaces(ParkingSpaceRepository repository) {
+  print('\nDu har valt att hantera Parkeringsplatser.');
+  print('1. Skapa ny parkeringsplats');
+  print('2. Visa alla parkeringsplatser');
+  print('3. Uppdatera parkeringsplats');
+  print('4. Ta bort parkeringsplats');
+  print('5. Gå tillbaka till huvudmenyn');
+  stdout.write('Välj ett alternativ (1-5): ');
+  var choice = stdin.readLineSync();
+
+  switch (choice) {
+    case '1':
+      stdout.write('Ange ID: ');
+      var id = stdin.readLineSync();
+      stdout.write('Ange adress: ');
+      var address = stdin.readLineSync();
+      stdout.write('Ange pris per timme: ');
+      var pricePerHour = double.tryParse(stdin.readLineSync() ?? '0');
+      var parkingSpace = ParkingSpace(id: id!, address: address!, pricePerHour: pricePerHour!);
+      repository.add(parkingSpace);
+      print('Parkeringsplats skapad: $parkingSpace');
+      break;
+    case '2':
+      var parkingSpaces = repository.getAll();
+      if (parkingSpaces.isEmpty) {
+        print('Inga parkeringsplatser registrerade.');
+      } else {
+        parkingSpaces.forEach((parkingSpace) => print(parkingSpace));
+      }
+      break;
+    case '3':
+      stdout.write('Ange ID för den parkeringsplats du vill uppdatera: ');
+      var id = stdin.readLineSync();
+      var parkingSpace = repository.getById(id!);
+      if (parkingSpace != null) {
+        stdout.write('Ange ny adress: ');
+        var newAddress = stdin.readLineSync();
+        stdout.write('Ange nytt pris per timme: ');
+        var newPricePerHour = double.tryParse(stdin.readLineSync() ?? '0');
+        parkingSpace.address = newAddress!;
+        parkingSpace.pricePerHour = newPricePerHour!;
+        repository.update(parkingSpace);
+        print('Parkeringsplats uppdaterad: $parkingSpace');
+      } else {
+        print('Ingen parkeringsplats hittades med det ID:et.');
+      }
+      break;
+    case '4':
+      stdout.write('Ange ID för den parkeringsplats du vill ta bort: ');
+      var id = stdin.readLineSync();
+      repository.delete(id!);
+      print('Parkeringsplats borttagen.');
+      break;
+    case '5':
+      return;
+    default:
+      print('Ogiltigt val, försök igen.');
+  }
+}
+
+void manageParkings(ParkingRepository parkingRepo, VehicleRepository vehicleRepo, ParkingSpaceRepository parkingSpaceRepo) {
+  print('\nDu har valt att hantera Parkeringar.');
+  print('1. Skapa ny parkering');
+  print('2. Visa alla parkeringar');
+  print('3. Uppdatera parkering');
+  print('4. Ta bort parkering');
+  print('5. Gå tillbaka till huvudmenyn');
+  stdout.write('Välj ett alternativ (1-5): ');
+  var choice = stdin.readLineSync();
+
+  switch (choice) {
+    case '1':
+      stdout.write('Ange fordonets registreringsnummer: ');
+      var regNumber = stdin.readLineSync();
+      var vehicle = vehicleRepo.getByRegistrationNumber(regNumber!);
+      if (vehicle != null) {
+        stdout.write('Ange parkeringsplatsens ID: ');
+        var parkingSpaceId = stdin.readLineSync();
+        var parkingSpace = parkingSpaceRepo.getById(parkingSpaceId!);
+        if (parkingSpace != null) {
+          var parking = Parking(vehicle: vehicle, parkingSpace: parkingSpace, startTime: DateTime.now(), endTime: null);
+          parkingRepo.add(parking);
+          print('Parkering skapad: $parking');
+        } else {
+          print('Ingen parkeringsplats hittades med det ID:et.');
+        }
+      } else {
+        print('Inget fordon hittades med det registreringsnumret.');
+      }
+      break;
+    case '2':
+      var parkings = parkingRepo.getAll();
+      if (parkings.isEmpty) {
+        print('Inga parkeringar registrerade.');
+      } else {
+        parkings.forEach((parking) => print(parking));
+      }
+      break;
+    case '3':
+      stdout.write('Ange fordonets registreringsnummer för den parkering du vill uppdatera: ');
+      var regNumber = stdin.readLineSync();
+      var parking = parkingRepo.getByVehicleRegistration(regNumber!);
+      if (parking != null) {
+        stdout.write('Vill du avsluta parkeringen? (j/n): ');
+        var shouldEnd = stdin.readLineSync();
+        if (shouldEnd == 'j') {
+          parking.endTime = DateTime.now();
+          parkingRepo.update(parking);
+          print('Parkering uppdaterad: $parking');
+        }
+      } else {
+        print('Ingen parkering hittades för det fordonet.');
+      }
+      break;
+    case '4':
+      stdout.write('Ange fordonets registreringsnummer för den parkering du vill ta bort: ');
+      var regNumber = stdin.readLineSync();
+      parkingRepo.delete(regNumber!);
+      print('Parkering borttagen.');
       break;
     case '5':
       return;
